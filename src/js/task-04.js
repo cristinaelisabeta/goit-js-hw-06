8b1da00703ad97656177381ab6de58501321096d
cristinaelisabeta <119240018+cristinaelisabeta@users.noreply.github.com>
2023-05-02 15:13:54 +0300

    tema 6

---
+++ src/js/task-04.js
@@ -0,0 +1,17 @@
+const counter = document.getElementById('counter');
+const valueSpan = document.getElementById('value');
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+
+let counterValue = 0;
+valueSpan.textContent = counterValue;
+
+decrementBtn.addEventListener('click', () => {
+  counterValue--;
+  valueSpan.textContent = counterValue;
+});
+
+incrementBtn.addEventListener('click', () => {
+  counterValue++;
+  valueSpan.textContent = counterValue;
+})
